b6f45b617d490efe2376316d69d0f6cbd9aa44c0
Hacker Hippo <37157257+hacker-hippo@users.noreply.github.com>
2024-11-14 23:55:03 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { ConsoleTransition } from './components/ConsoleTransition';
+
+function App() {
+  return (
+    <div className="App">
+      <ConsoleTransition />
+    </div>
+  );
+}
+
+export default App;
